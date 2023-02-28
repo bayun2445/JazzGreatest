12b5df0143efa1ff035642863171ba3f0e79fef8
bayun2445 <52805985+bayun2445@users.noreply.github.com>
2023-02-28 10:56:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jazzgreatest/AboutActivity.kt
@@ -0,0 +1,15 @@
+package com.example.jazzgreatest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jazzgreatest.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAboutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
