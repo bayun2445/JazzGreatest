66c062ffeba8f60c257dc4fec356dcb93e4f4cd7
bayun2445 <52805985+bayun2445@users.noreply.github.com>
2023-03-04 01:31:04 +0700

    Added click listener to detail activity

--- app/src/main/java/com/example/jazzgreatest/DetailActivity.kt
+++ app/src/main/java/com/example/jazzgreatest/DetailActivity.kt
@@ -1,23 +1,55 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.jazzgreatest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.jazzgreatest.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
+    private lateinit var legend: Legend
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setButtonBackClickListener()
+        loadLegendDetail()
+        setButtonClickListener()
     }
 
-    private fun setButtonBackClickListener() {
+    private fun loadLegendDetail() {
+        legend = intent.getParcelableExtra("legend")!!
+
+        binding.ivLegendPhoto.setImageResource(legend.imageId!!)
+        binding.tvLegendName.text = legend.name
+        binding.tvBirthName.text = legend.birthName
+        binding.tvBornDate.text = legend.born
+        binding.tvDiedDate.text = legend.died
+        binding.tvYearsActive.text = legend.yearsActive
+        binding.tvInstrumentText.text = legend.instrument
+        binding.tvPopularSong.text = legend.popularSong
+        binding.tvSummary.text = legend.summary
+    }
+
+    private fun setButtonClickListener() {
         binding.btnBack.setOnClickListener {
             finish()
+        }
+
+        binding.btnShare.setOnClickListener {
+            val nameQuery = legend.name?.replace(" ", "+")
+            val songQuery = legend.popularSong?.replace(" ", "+")
+            val youtubeQuery = "https://www.youtube.com/results?search_query=$songQuery+$nameQuery"
+            val shareBody = "Hello! I'd like to share my favorite Jazz Legend. He/she is ${legend.name} who has popular song \"${legend.popularSong}\" \n\n Check it out on YouTube! $youtubeQuery"
+
+            val shareIntent = Intent(Intent.ACTION_SEND).setType("text/plain")
+                .putExtra(Intent.EXTRA_SUBJECT, "Jazz Greatest")
+                .putExtra(Intent.EXTRA_TEXT, shareBody)
+
+            startActivity(Intent.createChooser(shareIntent, "Share to"))
         }
     }
 }
--- app/src/main/java/com/example/jazzgreatest/LegendData.kt
+++ app/src/main/java/com/example/jazzgreatest/LegendData.kt
@@ -69,14 +69,14 @@ object LegendData {
     private val instruments = arrayOf(
         "Piano",
         "Piano",
-        "Trumpet, Flugelhorn, Cornet",
+        "Trumpet, Flugelhorn",
         "Piano",
-        "Trumpet, Flugelhorn, Cornet",
+        "Trumpet, Flugelhorn",
         "Double Bass, Piano",
         "Piano",
         "Piano, Organ",
         "Piano",
-        "Piano, Keyboard, Drums",
+        "Piano, Keyboard",
     )
 
     private val popularSongs = arrayOf(
@@ -98,9 +98,9 @@ object LegendData {
         "Miles Davis was an American jazz trumpeter, bandleader, and composer who was one of the most influential figures in the history of jazz. Davis was at the forefront of several major developments in the genre, including bebop, cool jazz, and jazz fusion. He was known for his virtuosic playing style, innovative use of improvisation and his ability to assemble talented ensembles. Davis released numerous acclaimed albums throughout his career, including Kind of Blue, which is widely regarded as one of the greatest jazz albums of all time.",
         "Billy Strayhorn was an American jazz composer, pianist, and arranger who is best known for his close collaboration with Duke Ellington. Strayhorn wrote many of Ellington's most famous compositions, including \"Take the A Train,\" and was a key member of the Duke Ellington Orchestra. He was known for his sophisticated arrangements and unique harmonic sense, which helped to shape the sound of jazz during the mid-20th century. Despite his significant contributions to jazz, Strayhorn remains somewhat underappreciated in the wider world of music.",
         "Thad Jones was an American jazz trumpeter, composer, and bandleader who was a prominent figure in the jazz scene during the mid-20th century. Jones was known for his virtuosic trumpet playing and his innovative compositions, which often featured complex harmonies and rhythms. He led his own successful big band for many years and was also a prolific arranger for other musicians. Jones is widely regarded as one of the most important jazz musicians of his generation.",
+        "Charles Mingus was an American jazz double bassist, composer, and bandleader who was one of the most influential figures in the history of jazz. Mingus was known for his virtuosic bass playing, his innovative compositions, and his strong social and political views, which often found expression in his music. He led several successful ensembles during his career and recorded numerous acclaimed albums, including The Black Saint and the Sinner Lady. Mingus is widely regarded as one of the greatest jazz musicians of all time.",
         "Bill Evans was an influential American jazz pianist and composer who played a significant role in the development of modern jazz piano. He was known for his unique playing style, characterized by a delicate touch, complex harmonies, and introspective sound. Evans collaborated with many acclaimed jazz musicians and recorded numerous successful albums, including \"Sunday at the Village Vanguard.\" His contributions to jazz have made him widely regarded as one of the most important jazz pianists of all time.",
         "Count Basie was a highly influential American jazz pianist, composer, and bandleader who played a major role in the swing era of jazz. He led a band that was known for its tight, rhythmic sound and its focus on improvisation rooted in the blues. Basie recorded numerous highly-regarded albums throughout his career, including \"One O'Clock Jump\" and \"April in Paris.\" He was also a mentor to many younger musicians, and his impact on jazz can be heard in the work of countless other artists who followed in his footsteps.",
-        "Charles Mingus was an American jazz double bassist, composer, and bandleader who was one of the most influential figures in the history of jazz. Mingus was known for his virtuosic bass playing, his innovative compositions, and his strong social and political views, which often found expression in his music. He led several successful ensembles during his career and recorded numerous acclaimed albums, including The Black Saint and the Sinner Lady. Mingus is widely regarded as one of the greatest jazz musicians of all time.",
         "Mary Lou Williams was an influential American jazz pianist, composer, and arranger who played a key role in the development of bebop and other modern jazz styles. Known for her exceptional piano skills and innovative compositions, Williams collaborated with many renowned jazz musicians and recorded numerous highly-regarded albums, such as \"Zodiac Suite.\" Additionally, she served as an important mentor and teacher to other musicians, particularly women, and played an instrumental role in shaping the jazz scene of her time.",
         "Chick Corea was an American jazz pianist, composer, and bandleader who was one of the most influential figures in the development of jazz fusion during the late 20th century. Corea was known for his virtuosic keyboard playing, his innovative compositions that blended elements of jazz, rock, and classical music, and his leadership of several successful ensembles. He recorded numerous acclaimed albums throughout his career, including \"Return to Forever,\" and won 23 Grammy awards. Corea passed away in 2021 but his legacy continues to inspire musicians worldwide."
     )
